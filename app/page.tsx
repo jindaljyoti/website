0fcf9b51cdb44f9f80877915580a99a71a012a82
Pranay <pranay@sprinto.com>
2023-10-20 16:40:06 +0530

    Add welcome message to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import ColorTemplate from "@/components/ColorTemplate"
+
 
 export default function Home() {
   return (
-   <div>
-    <ColorTemplate/>
+   <div className='text-gray-1000 font-bold text-2xl text-center mt-20 w-full'>
+    Hello world, welcome to @kotapi/rad-ui, where you can build amazing ui really fast for your web app.
    </div>
   )
 }
