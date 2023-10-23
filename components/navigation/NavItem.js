d3ab42691446d0d90895e64d2b2641d4d0125ea7
Pranay <pranay@sprinto.com>
2023-10-23 13:33:30 +0530

    Refactor hover styles in NavItem.js component

--- components/navigation/NavItem.js
+++ components/navigation/NavItem.js
@@ -5,7 +5,7 @@ const NavItem = ({ item }) => {
     const path = window.location.pathname;
     const isCurrentPath =  path===item.path;
     const baseItemClasses = "block px-3 py-2 text-sm  rounded-2xl mb-0.5 cursor-pointer"
-   const activeClasses = isCurrentPath?'bg-plum-300 font-medium text-gray-1000':'font-light text-gray-950'
+   const activeClasses = isCurrentPath?'bg-plum-300 font-medium text-gray-1000':'font-light text-gray-950 hover:bg-plum-100'
 
     return <li  className='block'>
         <a className={`${baseItemClasses} ${activeClasses}`} href={`${item.path}`}>{item.title}</a>
