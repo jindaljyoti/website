8ab2f7395736d31bef126773d2c2f0a94bffa4cd
Pranay <pranay@sprinto.com>
2023-12-03 17:15:54 +0530

    Refactor NavItem component and add usePathname hook in Navigation component

--- components/navigation/NavItem.js
+++ components/navigation/NavItem.js
@@ -1,23 +1,12 @@
-'use client'
+"use client"
 
-
-const NavItem = ({ item }) => {
-    const isClient = typeof window !== 'undefined';
-
-    // ignore if server
-
-    if (!isClient) {
-        return null;
-    }
-    
-    const path = typeof window !==undefined && window.location.pathname;
-    const isCurrentPath =  path===item.path;
+const NavItem = ({ item, path }) => {
+    const isCurrentPath =  path===item?.path;
     const baseItemClasses = "block px-3 py-2 text-sm  rounded-2xl mb-0.5 cursor-pointer"
    const activeClasses = isCurrentPath?'bg-plum-300 font-medium text-gray-1000':'font-light text-gray-950 hover:bg-plum-100'
 
-    return <li  className='block'>
-        <a className={`${baseItemClasses} ${activeClasses}`} href={`${item.path}`}>{item.title}</a>
-    </li>
+    return <a className={`${baseItemClasses} ${activeClasses}`} href={`${item?.path}`}>{item?.title}</a>
+    
 }
 
 export default NavItem;
