3306a8a8724d0fb5ad5905bb02868365bc7213be
Pranay <pranay@sprinto.com>
2023-12-04 00:57:31 +0530

    Add documentation navigation and code component

---
+++ app/docs/docsIndex.js
@@ -0,0 +1,45 @@
+const POSTFIX = " | Rad UI";
+
+const DOCS_SEO = {
+    "INTRODUCTION":{
+        title:`Introduction${POSTFIX}`,
+        basic_title:"Introduction",
+        next:"INSTALLATION",
+        description: `It's super easy to get started with Rad UI. You can install it using npm or yarn.`,
+        url:"/docs/introduction"
+    },
+    "INSTALLATION":{
+        title:`Installation${POSTFIX}`,
+        basic_title:"Installation",
+        next:"USAGE",
+        previous:"INTRODUCTION",
+        description: `It's super easy to get started with Rad UI. You can install it using npm or yarn.`,
+        url:"/docs/installation"
+    },
+    "USAGE":{
+        title:`Usage${POSTFIX}`,
+        basic_title:"Usage",
+        next: null,
+        previous:"INSTALLATION",
+        description: `It's super easy to get started with Rad UI. You can install it using npm or yarn.`,
+        url:"/docs/usage"
+    }
+}
+
+DOCS_SEO.getMetadata = (pageName)=>{
+    return DOCS_SEO[pageName]
+}
+
+DOCS_SEO.getNext = (pageName)=>{
+    const nextPageConstant = DOCS_SEO[pageName].next;
+    console.log(nextPageConstant)
+    return DOCS_SEO[nextPageConstant]
+}
+
+DOCS_SEO.getPrevious = (pageName)=>{
+    const nextPageConstant = DOCS_SEO[pageName].previous;
+    console.log(nextPageConstant)
+    return DOCS_SEO[nextPageConstant]
+}
+
+export default DOCS_SEO;
--- app/docs/installation/page.js
+++ app/docs/installation/page.js
@@ -1,9 +1,16 @@
+const PAGE_NAME = 'INSTALLATION'
+
+import SEO from "../docsIndex"
+export const metadata = SEO.getMetadata(PAGE_NAME)
+
+
+
 import Documentation from "@/components/layout/Documentation/Documentation"
 import { Text, Heading, Link, Code } from "@kotapi/rad-ui"
 
 
 const Introduction = ()=>{
-    return  <Documentation next={{title:""}} title="Installation">
+    return  <Documentation next={SEO.getNext(PAGE_NAME)} title="Installation">
         <Documentation.Section title="First steps">
             <Text className="mb-2 text-gray-1000 font-light">
                 It's super easy to get started with Rad UI. You can install it using npm or yarn.
@@ -11,7 +18,14 @@ const Introduction = ()=>{
 
             <Text className="mt-4 mb-1 font-medium">Using Yarn</Text>
             <Code>
-                npm install @kotapi/rad-ui
+                yarn add @kotapi/rad-ui
+            </Code>
+
+            {/*  */}
+
+            <Text className="mt-4 mb-1 font-medium">Using npm</Text>
+            <Code>
+                npm install @kotapi/rad-ui --save
             </Code>
         </Documentation.Section>
     </Documentation>
