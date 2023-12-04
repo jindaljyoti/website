57058c71546219c4876030d1ddb57aea2c326031
Pranay <pranay@sprinto.com>
2023-12-05 01:02:39 +0530

    Add syntax highlighting support for JSX code in CodeBlock component

--- components/layout/Documentation/helpers/CodeBlock.js
+++ components/layout/Documentation/helpers/CodeBlock.js
@@ -1,8 +1,59 @@
+import React from 'react';
+import { refractor } from 'refractor';
+import js from 'refractor/lang/javascript';
+import jsx from 'refractor/lang/jsx';
 
-const CodeBlock = ({children})=>{
-    return <div>
-        {children}
+refractor.register(js);
+refractor.register(jsx);
+
+
+const renderElement = (element) => {
+    if (element.type === 'element') {
+      const { tagName, properties, children } = element;
+      const className = properties.className.join(' ');
+
+      return React.createElement(
+        tagName,
+        { className },
+        children.map(renderElement)
+      );
+    } else if (element.type === 'text') {
+      return element.value;
+    } else {
+      return null;
+    }
+  };
+
+
+const CodeBlock = ({ children }) => {
+  let code = refractor.highlight(children, 'jsx');
+ 
+  const renderElement = (element) => {
+    if (element.type === 'element') {
+      const { tagName, properties, children } = element;
+      const className = properties.className.join(' ');
+
+      return React.createElement(
+        tagName,
+        { className },
+        children.map(renderElement)
+      );
+    } else if (element.type === 'text') {
+      return element.value;
+    } else {
+      return null;
+    }
+  };
+
+
+
+
+    code = code.children.map(renderElement);
+  return (
+    <div>
+      <code className='language-jsx'>{code}</code>
     </div>
-}
+  );
+};
 
 export default CodeBlock;
