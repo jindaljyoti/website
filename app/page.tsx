ce9e245e2811f324a3eea828ab0da4d473f241ed
kotAPI <pranaykothapalli@gmail.com>
2023-12-06 02:31:10 +0530

    add: music player demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,33 @@
 'use client'
 
-import { Button } from "@kotapi/rad-ui"
+import { Button, Heading } from "@kotapi/rad-ui"
+
+import MusicAppPlayerDemo from './landingComponents/MusicAppPlayerDemo'
 
 export default function Home() {
   return (
-    <div>
-      <div className='text-gray-1000 font-bold text-2xl text-center mt-20 w-full'>
-        Hello world, welcome to @kotapi/rad-ui, <br /> Build no compromise UI
-      </div>
-      <div className='flex justify-center mt-3 space-x-2'>
-        <Button color="plum" variant="outline" onClick={() => {
-          location.href = '/playground'
-        }}>Go to playground</Button>
+    <div className='min-h-screen'>
+      <div className='p-10 h-full'>
+        <div className='py-20 mb-10'>
+          <div>
+            <Heading className="text-center text-gray-1000">
+              <span className='text-crimson-950'>Build</span> no compromise <span className='text-blue-950'>UI</span> <br /> Fast.
+            </Heading>
+            <div className='flex justify-center mt-3 space-x-2'>
+              <Button color="blue" variant="soft" onClick={() => {
+                location.href = '/playground'
+              }}>Go to playground</Button>
 
-        <Button color="tomato" variant="solid" onClick={() => {
-          location.href = '/colors'
-        }}>Check out Colors</Button>
+              <Button color="blue" variant="ghost" onClick={() => {
+                location.href = '/colors'
+              }}>Check out Colors</Button>
+            </div>
+          </div>
+        </div>
+        {/*  */}
+        <div className='text-gray-1000 p-10 mt-10'>
+          <MusicAppPlayerDemo/>
+        </div>
       </div>
     </div>
   )
