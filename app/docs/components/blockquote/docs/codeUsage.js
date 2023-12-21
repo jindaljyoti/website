c0d13d3d1dd74c5ae427b3ee12386b54c43d3fb8
Pranay <pranay@sprinto.com>
2023-12-21 12:48:00 +0530

    Add Badge and BlockQuote components and their documentation pages

---
+++ app/docs/components/blockquote/docs/codeUsage.js
@@ -0,0 +1,19 @@
+const code = {
+    javascript: {
+        code: `import BlockQuote from "@rad-ui/ui/BlockQuote"
+
+const BlockQuoteExample = () => (
+    <div>
+        <BlockQuote> When my time comes, forget the wrong that I've done.
+        Help me leave behind some reasons to be missed.
+        Don't resent me and when you're feeling empty, keep me in your memory.
+        Leave out all the rest, leave out all the rest. </BlockQuote>
+    </div>
+)`
+    },
+    css: {
+        code: `todo`
+    },
+}
+
+export default code;
