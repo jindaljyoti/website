10c2825c38238b05331e11c27e2650f5eef1243c
jyoti <jindaljyoti913@gmail.com>
2024-09-05 16:56:16 -0700

    add description for switch

--- app/docs/components/switch/page.js
+++ app/docs/components/switch/page.js
@@ -10,7 +10,10 @@ import codeUsage from "./docs/codeUsage"
 
 const SwitchDocs = () => {
     return <div>
-        <Documentation currentPage={PAGE_NAME} title='Switch'>
+        <Documentation currentPage={PAGE_NAME} title='Switch' description={`
+            Switch is used to toggle between two states on and off. You can commonly used in settings panel, forms and any other 
+            place where a user needs to enable or disable feature.
+            `}>
             <Documentation.ComponentHero codeUsage={codeUsage}>
                 <Switch>On</Switch>
             </Documentation.ComponentHero>
